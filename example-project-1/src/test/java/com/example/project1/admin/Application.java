8646c45cb5a3f2a608820e3ae5fed26e0b8de97a
Junyong <jypark93@kookmin.ac.kr>
2022-12-27 00:18:10 +0900

    ex#1 : testCode 완성

---
+++ example-project-1/src/test/java/com/example/project1/admin/Application.java
@@ -0,0 +1,11 @@
+package com.example.project1.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static  void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+}
