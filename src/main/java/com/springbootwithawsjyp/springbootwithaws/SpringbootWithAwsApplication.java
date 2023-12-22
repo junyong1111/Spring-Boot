28e58bda61a92eac3debb46db3938bd66e94a554
Junyong <dak_kiwon@JunYong-MacBook-Pro.local>
2023-12-23 01:29:17 +0900

    feat : posts-restapi - List 조회 기능 추가

--- src/main/java/com/springbootwithawsjyp/springbootwithaws/SpringbootWithAwsApplication.java
+++ src/main/java/com/springbootwithawsjyp/springbootwithaws/SpringbootWithAwsApplication.java
@@ -3,7 +3,10 @@ package com.springbootwithawsjyp.springbootwithaws;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -11,6 +14,17 @@ public class SpringbootWithAwsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootWithAwsApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
