75adc14262800c3050f2243627b781fd15547ce0
Junyong <dak_kiwon@JunYong-MacBook-Pro.local>
2023-12-23 01:29:17 +0900

    feat : posts-restapi - Posts - GET, PUT, POST, DELETE API 기능 구현 완료

--- src/main/java/com/springbootwithawsjyp/springbootwithaws/web/PostsApiController.java
+++ src/main/java/com/springbootwithawsjyp/springbootwithaws/web/PostsApiController.java
@@ -1,13 +1,14 @@
 package com.springbootwithawsjyp.springbootwithaws.web;
 
-import com.springbootwithawsjyp.springbootwithaws.domain.posts.Posts;
-import com.springbootwithawsjyp.springbootwithaws.domain.posts.PostsRepository;
 import com.springbootwithawsjyp.springbootwithaws.service.PostsService;
+import com.springbootwithawsjyp.springbootwithaws.web.dto.PostsListResponseDto;
 import com.springbootwithawsjyp.springbootwithaws.web.dto.PostsResponseDto;
 import com.springbootwithawsjyp.springbootwithaws.web.dto.PostsSaveRequestDto;
 import com.springbootwithawsjyp.springbootwithaws.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,7 +20,10 @@ public class PostsApiController {
     public PostsResponseDto findById(@PathVariable long id){
         return postsService.findById(id);
     }
-
+    @GetMapping(value = "/api/v1/posts")
+    public List<PostsListResponseDto> findAll(){
+        return postsService.findAllDesc();
+    }
     @PostMapping(value = "/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
@@ -28,7 +32,10 @@ public class PostsApiController {
     @PutMapping(value = "/api/v1/posts/{id}")
     public Long update(@PathVariable long id, @RequestBody PostsUpdateRequestDto requestDto){
         return postsService.update(id, requestDto);
-
+    }
+    @DeleteMapping(value = "/api/v1/posts/{id}")
+    public void delete(@PathVariable long id){
+        postsService.delete(id);
     }
 
 
